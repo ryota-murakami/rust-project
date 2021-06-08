5fa553b3aee9b7b4c5c9c3996d350143142c3263
ryota-murakami <dojce1048@gmail.com>
2021-06-08 21:52:07 +0900

    done https://doc.rust-lang.org/book/ch01-03-hello-cargo.html

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/ryota.murakami/repository/rust-project/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/ryota.murakami/repository/rust-project/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/ryota.murakami/repository/rust-project/hello_cargo/target/debug/hello_cargo: /Users/ryota.murakami/repository/rust-project/hello_cargo/src/main.rs
